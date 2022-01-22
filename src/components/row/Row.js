0b9ba7abab5be8f19953ede3883b367ae13f1e2f
Your Name <drastic87@gmail.com>
2022-01-22 18:20:42 -0500

    Added react-slick, css changes in Row

--- src/components/row/Row.js
+++ src/components/row/Row.js
@@ -1,9 +1,51 @@
 import React, { useState, useEffect } from 'react';
 import { getMovies } from '../../requests/requests';
 import './row.css';
+import YouTube from 'react-youtube';
+import opts from '../../utils/youtubeOptions';
+import movieTrailer from 'movie-trailer';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import Slider from 'react-slick';
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
+    let windowWidth = window.innerWidth;
+    let slides = windowWidth < 900 ? 3 : windowWidth < 1350 ? 5 : 7;
+    const base_url = 'https://image.tmdb.org/t/p/w500';
     const [movies, setMovies] = useState([]);
+    const [trailerUrl, setTrailerUrl] = useState('');
+    const [mouseOver, setMouseOver] = useState(false);
+    const [sliderSettings, setSliderSettings] = useState({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: slides,
+        slidesToScroll: 5,
+    });
+
+    function debounce(fn, ms) {
+        let timer;
+        return (_) => {
+            clearTimeout(timer);
+            timer = setTimeout((_) => {
+                timer = null;
+                fn.apply(this, arguments);
+            }, ms);
+        };
+    }
+
+    useEffect(() => {
+        const debouncedHandleResize = debounce(function handleResize() {
+            slides = windowWidth < 900 ? 3 : windowWidth < 1350 ? 5 : 7;
+            setSliderSettings({ ...sliderSettings, slidesToShow: slides });
+        }, 50);
+
+        window.addEventListener('resize', debouncedHandleResize);
+
+        return (_) => {
+            window.removeEventListener('resize', debouncedHandleResize);
+        };
+    });
 
     useEffect(() => {
         async function fetchData() {
@@ -13,26 +55,73 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         fetchData(fetchUrl);
     }, [fetchUrl]);
 
-    const base_url = 'https://image.tmdb.org/t/p/w500';
-    return (
-        <div className="row">
-            <h2>{title}</h2>
-            <div className="row_posters">
-                {movies.map((movie) => (
-                    <img
-                        key={movie.id}
-                        className={`row_poster ${
-                            isLargeRow && 'row_posterLarge'
-                        }`}
-                        src={`${base_url}${
-                            isLargeRow ? movie.poster_path : movie.backdrop_path
-                        }`}
-                        alt={movie.name}
-                    />
-                ))}
-            </div>
+    //This code fixes issue with onClick executing while dragging movies.
+    let drag = false;
+    document.addEventListener('mousedown', () => (drag = false));
+    document.addEventListener('mousemove', () => (drag = true));
 
-            {/* {container->posters} */}
+    const handleClick = async (movie) => {
+        if (!drag) {
+            if (trailerUrl) {
+                setTrailerUrl('');
+            } else {
+                try {
+                    let res = await movieTrailer(null, {
+                        tmdbId: movie.id,
+                        id: true,
+                    });
+                    if (res === null) {
+                        res = await movieTrailer(
+                            movie?.name || movie?.title || movie?.original_name,
+                            {
+                                id: true,
+                            }
+                        );
+                    }
+                    setTrailerUrl(res);
+                } catch (err) {
+                    console.log(err);
+                }
+            }
+        }
+    };
+
+    return (
+        <div
+            className={`${
+                windowWidth > 900 ? 'slider-wrapper-large' : 'slider-wrapper'
+            }`}
+        >
+            <h2>{title}</h2>
+
+            <Slider {...sliderSettings}>
+                {movies.map((movie) => (
+                    <div className="slick-slide section" key={movie.id}>
+                        <img
+                            className={`slick-slide-image ${
+                                mouseOver && 'slick-slide-image-margin'
+                            }`}
+                            src={`${base_url}${
+                                isLargeRow
+                                    ? movie.poster_path
+                                    : movie.backdrop_path
+                            }`}
+                            onClick={() => handleClick(movie, drag)}
+                            onMouseOver={() => {
+                                setMouseOver(!mouseOver);
+                                console.log(mouseOver);
+                            }}
+                            onMouseOut={() => {
+                                setMouseOver(!mouseOver);
+                                console.log(mouseOver);
+                            }}
+                            alt={''}
+                        />
+                    </div>
+                ))}
+            </Slider>
+
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
         </div>
     );
 };
---
+++ src/utils/youtubeOptions.js
@@ -0,0 +1,9 @@
+const opts = {
+    height: '390',
+    width: '640',
+    playerVars: {
+        // https://developers.google.com/youtube/player_parameters
+        autoplay: 1,
+    },
+};
+export default opts;
