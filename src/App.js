1460b5ad5e351d03de23bb0ef3269e344fa33ce3
Your Name <drastic87@gmail.com>
2022-02-28 21:51:09 -0500

    Login/Register components created

--- src/App.js
+++ src/App.js
@@ -3,24 +3,28 @@ import './App.scss';
 import Row from './components/row/Row';
 import Banner from './components/banner/Banner';
 import Nav from './components/nav/Nav';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
 
 function App() {
     return (
-        <div className="App">
-            <Nav />
-            <Banner fetchUrl={'fetchNetflixOriginals'} />
-            <Row
-                title="NETFLIX ORIGINALS"
-                fetchUrl={'fetchNetflixOriginals'}
-                isLargeRow
-            />
-            <Row title="Trending Now" fetchUrl={'fetchtrending'} />
-            <Row title="Top Rated" fetchUrl={'fetchtoprated'} />
-            <Row title="Action Movies" fetchUrl={'fetchactionmovies'} />
-            <Row title="Comedy Movies" fetchUrl={'fetchcomedymovies'} />
-            <Row title="Horror Movies" fetchUrl={'fetchHorrorMovies'} />
-            <Row title="Documentaries" fetchUrl={'fetchDocumentaries'} />
-        </div>
+        <Login />
+
+        // <div className="App">
+        //     <Nav />
+        //     <Banner fetchUrl={'fetchNetflixOriginals'} />
+        //     <Row
+        //         title="NETFLIX ORIGINALS"
+        //         fetchUrl={'fetchNetflixOriginals'}
+        //         isLargeRow
+        //     />
+        //     <Row title="Trending Now" fetchUrl={'fetchtrending'} />
+        //     <Row title="Top Rated" fetchUrl={'fetchtoprated'} />
+        //     <Row title="Action Movies" fetchUrl={'fetchactionmovies'} />
+        //     <Row title="Comedy Movies" fetchUrl={'fetchcomedymovies'} />
+        //     <Row title="Horror Movies" fetchUrl={'fetchHorrorMovies'} />
+        //     <Row title="Documentaries" fetchUrl={'fetchDocumentaries'} />
+        // </div>
     );
 }
 
--- src/requests/requests.js
+++ src/requests/requests.js
@@ -2,7 +2,9 @@ import axios from 'axios';
 
 export const getMovies = async (fetchUrl) => {
     try {
-        const request = await axios.get(`/api/movies/${fetchUrl}`);
+        const request = await axios.get(
+            `http://localhost:5000/api/movies/${fetchUrl}`
+        );
         return request;
     } catch (err) {
         console.error(err);
